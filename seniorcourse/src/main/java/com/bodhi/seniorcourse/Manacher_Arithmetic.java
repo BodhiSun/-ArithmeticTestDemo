abe76ff2e9ccc3ec82a2fad9da46e01c02db2189
SunGQ <1207508396@qq.com>
2019-08-20 00:32:37 +0800

    求字符串中最大回文字符串长度

--- seniorcourse/src/main/java/com/bodhi/seniorcourse/Manacher_Arithmetic.java
+++ seniorcourse/src/main/java/com/bodhi/seniorcourse/Manacher_Arithmetic.java
@@ -8,7 +8,7 @@ package com.bodhi.seniorcourse;
  * 时间复杂度0(n)
  *
  * 回文直径：以一个中心开始向两边扩 能扩出来的长度范围。 回文半径即一半(包含中心)
- * 最右回文右边界：起始位置在-1 然后有回文就是最右边的字符 没回文就是自身点 边界位置不往回走 依次向后移动
+ * 最右回文右边界：起始位置在-1 然后有回文就是最右边的字符 没回文就是自身点 边界位置不往回走 依次向后移动求出每个字符
  * 最右回文右边界中心：第一次到达 最右回文右边界时的 中心点位置
  *
  *
@@ -26,4 +26,49 @@ package com.bodhi.seniorcourse;
  *
  */
 public class Manacher_Arithmetic {
+
+
+    /**
+     * manacher算法 求出字符串中最长回文子串的长度
+     * @param str
+     * @return
+     */
+    public static int maxLcpsLength(String str){
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        //处理字符串成带求解样式
+        char[] charArr = manacherString(str);
+        //存放每个字符的最大回文串长度
+        int[] pArr= new int[charArr.length];
+        int inde=-1;
+        int pR=-1;//最右回文右边界
+
+        return -1;
+
+
+
+
+    }
+
+
+    /**
+     * 将每个字符前后加上统一的任意符号
+     * @param str
+     * @return
+     */
+    public static char[] manacherString(String str){
+        char[] charArr = str.toCharArray();
+        char[] res = new char[str.length()*2+1];
+        int index=0;
+        for (int i=0;i!=res.length;i++){
+            res[i]=(i&1)==0?'#':charArr[index++];
+        }
+        return res;
+    }
+
+
+
+
+
 }
