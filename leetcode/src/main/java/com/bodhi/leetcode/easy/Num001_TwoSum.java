9cdfae04d9ef582daf29fead80e055e8bb9e52d3
sunguoqing01 <sunguoqing01@58.com>
2021-03-01 21:19:51 +0800

    1两数之和

--- leetcode/src/main/java/com/bodhi/leetcode/easy/Num001_TwoSum.java
+++ leetcode/src/main/java/com/bodhi/leetcode/easy/Num001_TwoSum.java
@@ -1,9 +1,32 @@
 package com.bodhi.leetcode.easy;
 
 /**
- * @author：Sun(孙国庆)
+ * @author：Sun
  * @date：2/25/21
- * @description：
+ * @desc: 两数之和
+ *
+ * 【题目】
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 你可以按任意顺序返回答案。
+ * eg:
+ * 输入：nums = [2,7,11,15], target = 9  输出：[0,1]
+ * 输入：nums = [3,3], target = 6  输出：[0,1]
+ *
  */
 public class Num001_TwoSum {
+
+    public static int[] towSum(int[] nums, int target){
+
+
+
+        return null;
+    }
+
+    public static void test(){
+        int[] array = new int[]{2,7,11,15};
+        int target = 9;
+        System.out.println(towSum(array,target).toString());
+    }
+
 }
